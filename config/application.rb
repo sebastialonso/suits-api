028d82db57f6368055e397259bfe7ddc1928c09c
Sebastian Gonzalez <sebagonz91@gmail.com>
2016-04-02 11:22:21 -0300

    Rails cors

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,12 @@ module SuitsApi
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.insert_before 0, "Rack::Cors", logger: (-> {Rails.logger}) do
+      allow do
+        #angular is :8000
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete], max_age: 20
+      end
+    end
   end
 end
